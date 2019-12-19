34ffbc38ad11b657f230709ab7f0370c24abf742
David Cast <dcastellanosc1300@alumno.ipn.mx>
2019-12-18 21:52:03 -0600

    Modify settings.py
    Se añadieron las configuraciones de la zona horaría, el idioma del sitio, la ruta para los archivos estáticos y los host.

--- Tutorial/DjangoGirls/misitio/misitio/settings.py
+++ Tutorial/DjangoGirls/misitio/misitio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*tb1w=&u-9&ibm^(_ji2l62_0a5r*pmq9vh&*t1dy^4bjdi*r%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
