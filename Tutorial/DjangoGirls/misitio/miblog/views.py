b479de2b16b75da6cabdc4ae3032d882c336d207
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-01-07 21:37:34 -0600

    Add a template view
    Se añadio un HTML para que se pueda mostrar como plantilla.

--- Tutorial/DjangoGirls/misitio/miblog/views.py
+++ Tutorial/DjangoGirls/misitio/miblog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'miblog/post_list.html', {})
