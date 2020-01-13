532eae3f491b27f2f62f19eb0783bdabaaff8cd7
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-01-13 13:20:46 -0600

    Add Forms
    Se añade una mejor vista para agregar nuevos elementos con formularios sin tener la necesitad de utilizar siempre la vista de Admin.

--- Tutorial/DjangoGirls/misitio/miblog/views.py
+++ Tutorial/DjangoGirls/misitio/miblog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -12,3 +14,16 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'miblog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'miblog/post_edit.html', {'form': form})
