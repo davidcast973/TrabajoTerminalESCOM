3c7bc803a196802f6d2e3e18eb01ec3a3a1266b9
Oscar Cruces <ocrucesl1000@alumno.ipn.mx>
2020-02-10 13:49:10 -0600

    Login e integración Bootstrap 4
    Se reoganisaron directorios y se integró bootstrap

--- SASTRE/sastre/settings.py
+++ SASTRE/sastre/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'sastre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
