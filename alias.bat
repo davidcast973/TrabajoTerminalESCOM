ad4c134af7f026f77a335fe6e05276811ca86175
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-02-11 21:21:24 -0600

    Actualizar rama
    Se agregan los archivos que son necesarios para instalar el proyecto de la rama en la que se está trabajando.

---
+++ alias.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+doskey ls= dir /b
+doskey irtt= E: ^& cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE
+doskey actvenv= cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE\sastreenv ^&^& cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE ^&^& E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE\sastreenv\Scripts\activate
