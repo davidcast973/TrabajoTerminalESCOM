3e5a3077738bd3c2421358fb03ec2e87e7901629
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-02-18 20:53:10 -0600

    Crear plantillas extendidas
    Se crea un marco para replicar en las plantillas que no sean base.

--- SASTRE/bosque/views.py
+++ SASTRE/bosque/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def bosque_list(request):
-    return render(request, 'bosque/bosque_list.html', {})
+	animales= Animal.objects.all().order_by('nombreAnimal')
+	return render(request, 'bosque/bosque_list.html', {'animales': animales})
