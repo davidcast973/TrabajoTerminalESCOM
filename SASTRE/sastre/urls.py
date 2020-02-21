a6e8f518d878bb8ec95b51ae3339c382ff1e23a6
Oscar Cruces <ocrucesl1000@alumno.ipn.mx>
2020-02-21 11:30:40 -0600

    Eliminaciónvde app ControlSesion
    Eliminación de app controlSesion

--- SASTRE/sastre/urls.py
+++ SASTRE/sastre/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', include('controlSesion.urls')),
-	#Add Django site authentication urls (for login, logout, password management)
-    path('cuentas/', include('django.contrib.auth.urls')),
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    #Add Django site authentication urls (for login, logout, password management)
+    path('cuneta/', include('django.contrib.auth.urls')),
     path('usuario/', include('usuario.urls')),
 ]
