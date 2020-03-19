7964d98f8b5992f83db29ffa458fc5393027cb0c
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-03-19 00:31:18 -0600

    Cabiar nombre de BD
    PostgreSQL no acepta nombre muy largos.

--- CREATE USER.sql
+++ CREATE USER.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS ttsastrePrueba;
-DROP USER IF EXISTS susastrePrueba;
-CREATE DATABASE ttsastrePrueba;
-CREATE USER susastrePrueba WITH PASSWORD 'susastrePrueba';
-GRANT ALL PRIVILEGES ON DATABASE ttsastrePrueba to susastrePrueba;
+DROP DATABASE IF EXISTS ttprueba;
+DROP USER IF EXISTS suprueba;
+CREATE DATABASE ttprueba;
+CREATE USER suprueba WITH PASSWORD 'suprueba';
+GRANT ALL PRIVILEGES ON DATABASE ttprueba to suprueba;
