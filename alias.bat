250d412266ee4c5580209e6cebd0893d33a5bb90
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-03-23 04:25:22 -0600

    Hacer correciones
    * Hacer correciones de textos
    * Replicar la plantilla base en las demás paginas de registration.

--- alias.bat
+++ alias.bat
@@ -5,3 +5,7 @@ doskey irtt= E: ^& cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE
 doskey vir= cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE\sastreenv ^&^& cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE ^&^& E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE\sastreenv\Scripts\activate
 doskey correr= python manage.py runserver
 doskey cambios= python manage.py makemigrations ^&^& python manage.py migrate
+
+E:
+cd E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE
+E:\Mi\Mavis\Github\TrabajoTerminalESCOM\SASTRE\sastreenv\Scripts\activate
