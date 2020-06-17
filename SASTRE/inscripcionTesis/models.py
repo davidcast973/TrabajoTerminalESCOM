9d7bfa71cad2236ff33e802737aa4342e0c8ed77
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-06-17 05:14:25 -0500

    Mejorar script para poblar las tesis
    Se añaden métodos para que las fechas sean iguales, en caso de que no exista fecha se pondrá la que por lo regular es la primera en los dispositivos (1920-01-01). 
    Ya se puede poblar la mayoría de las materias. Conflictos con las fechas DD/MM/AAAA.

--- SASTRE/inscripcionTesis/models.py
+++ SASTRE/inscripcionTesis/models.py
@@ -10,14 +10,14 @@ class Tesis(models.Model):
 	"""
 
 	numeroTesis= models.IntegerField(default=00000)
-	nombreTesis= models.CharField("Titulo de la tesis",max_length= 500, default= "")
-	alumnoAps= models.CharField("Apeido(s) del alumno autor",max_length= 100, default= "")
-	alumno= models.CharField("Nombre del alumno autor",max_length= 100, default= "")
+	nombreTesis= models.CharField("Titulo de la tesis", max_length= 500, default= "")
+	alumnoAps= models.CharField("Apeido(s) del alumno autor", max_length= 100, default= "")
+	alumno= models.CharField("Nombre del alumno autor", max_length= 100, default= "")
 	director1Aps= models.CharField("Apeido(s) de director 1", max_length=50, default= "")
 	director1= models.CharField("Nombre de director 1", max_length=50, default= "")
-	abstrac= models.CharField("Abstrac de la tesis",max_length=5000, default= "", null=True)
+	abstrac= models.TextField("Abstrac de la tesis", default= "", null=True)
 	diaCreacion= models.DateField(default='1920-01-01', null=True)
-	valorW2V= models.FloatField(default= 0)
+	valorW2V= models.TextField("Valor obtenido de word2vec", default= ','.join([str(0) for x in range(300)]), null=True)
 
 	def __str__(self):
 		return str(self.nombreTesis)
--- poblarTesis.py
+++ poblarTesis.py
@@ -1,15 +1,16 @@
 """
-	Hay que descargar primero los Stopwords antes de seleccionar el idioma español con:
-	nltk.download('stopwords')
+   Hay que descargar primero los Stopwords antes de seleccionar el idioma español con:
+   nltk.download('stopwords')
 """
 
 import pickle as p # Serializar
-from nltk.corpus import stopwords # Tokenizar
+#from nltk.corpus import stopwords # Tokenizar
 from nltk import word_tokenize # Tokenizar
 from string import punctuation # Tokenizar
 from inscripcionTesis.models import Tesis as T
 import gensim # Word2Vec
-from sklearn.metrics.pairwise import cosine_similarity as cs # Similitud coseno
+#from sklearn.metrics.pairwise import cosine_similarity as cs # Similitud coseno
+from datetime import datetime # Castear a fecha AAAA-MM-DD
 
 model = gensim.models.KeyedVectors.load_word2vec_format('sbw_vectors.bin', binary=True) # Word2Vec
 
@@ -17,43 +18,62 @@ file= open('todasTesis.pickle','rb')
 resumenes= p.load(file)
 file.close()
 
-spanish_stopwords= stopwords.words('spanish')
+#nltk.download('stopwords') #Descargar elementos necesarios para el filtrado de stopwords
+#spanish_stopwords= stopwords.words('spanish')
 
 #Para remover la puntuacion
 non_words = list(punctuation)
 #Agregamos puntuacion usada en el idioma español
-non_words.extend(['¿','¡'])
+non_words.extend(['¿','¡','\n','\r'])
 non_words.extend(map(str,range(10)))
 
-cont= 0
+#cont= 0
 for x in resumenes:
-	caracteres= ''.join([c for c in x[4] if c not in non_words])
-	
-	#AQUI SE NECESITA PRIMERO LA DESCARGA
-	tokens = word_tokenize(caracteres)
-	#tokens_filtrados = [token for token in tokens if token not in spanish_stopwords]
-	if not tokens:
-		w2v= [0 for x in range(300)]
-	else:
-		w2v= [0 for x in range(300)]
-		for w in tokens:
-			#print(len(tokens),tokens)
-			#print(len(w2v),w2v)
-			if w in model:
-				w2v+= model[w]
-	print(len(w2v),w2v)
-	tesis= T()
-	tesis.numeroTesis= int(x[0]) if x[0]!= '' else 0
-	tesis.nombreTesis= x[1] if x[1]!= '' else ""
-	tesis.alumnoAps= x[2].split(', ')[0] if x[2]!= '' else ""
-	tesis.alumno= x[2].split(', ')[1] if x[2]!= '' else ""
-	tesis.director1Aps= x[3].split(', ')[0] if x[3]!= '' else ""
-	tesis.director1= x[3].split(', ')[1] if x[3]!= '' else ""
-	tesis.abstrac= x[4] if x[4]!= '' else ""
-	tesis.diaCreacion= x[5] if x[5]!= '' else '1920-01-01'
-	tesis.valorW2V= w2v
-	tesis.save()
-	if cont<2:
-		cont+= 1
-	else:
-		break
+   texto= list()
+   for c in x[4]:
+      if c not in non_words:
+         texto.append(c)
+      elif c == '\n':
+         texto.append(' ')
+   caracteres= ''.join(texto)
+   #AQUI SE NECESITA PRIMERO LA DESCARGA
+   tokens = word_tokenize(caracteres)
+   #tokens_filtrados = [token for token in tokens if token not in spanish_stopwords]
+   if not tokens:
+      w2v= ','.join([str(0) for x in range(300)])
+   else:
+      w2v= [0 for x in range(300)]
+      for w in tokens:
+         #print(len(tokens),tokens)
+         #print(len(w2v),w2v)
+         if w in model:
+            w2v+= model[w]
+   print(len(w2v),w2v)
+   tesis= T()
+   tesis.numeroTesis= int(x[0]) if x[0]!= '' else 0
+   tesis.nombreTesis= x[1] if x[1]!= '' else ""
+   tesis.alumnoAps= x[2].split(', ')[0] if x[2]!= '' else ""
+   tesis.alumno= x[2].split(', ')[1] if x[2]!= '' else ""
+   tesis.director1Aps= x[3].split(', ')[0] if x[3]!= '' else ""
+   tesis.director1= x[3].split(', ')[1] if x[3]!= '' else ""
+   tesis.abstrac= x[4] if x[4]!= '' else ""
+   if len(x[5])==10:   # AAAA-MM-DD -> 10 o DD/MM/AAAA
+      if '-' in [c for c in x[5]]:
+         tesis.diaCreacion= x[5]
+      else:
+         tesis.diaCreacion= str(datetime.strptime(x[5], '%d/%m/%Y'))[:11]
+   elif len(x[5])==7:   # AAAA-MM -> 7
+      tesis.diaCreacion= x[5]+'-01'
+   elif len(x[5])==4:   # AAAA -> 4
+      tesis.diaCreacion= x[5]+'-01-01'
+   elif len(x[5])==8:   # AAAA- MM -> 8
+      tesis.diaCreacion= x[5][:5]+x[5][6:]+'-01'
+   elif len(x[5])==0 or len(x[5])==9 :   #  -> 0 o 'sin fecha' -> 9
+      tesis.diaCreacion= '1920-01-01'
+   #tesis.diaCreacion= x[5] if x[5]!= '' else '1920-01-01'
+   tesis.valorW2V= ','.join([str(w) for w in w2v])
+   tesis.save()
+#   if cont<2:
+#      cont+= 1
+#   else:
+#      break
