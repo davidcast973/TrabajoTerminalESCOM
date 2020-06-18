c9b7d536ad6d868a1c62677f6395a8225ce1607a
David Cast <dcastellanosc1300@alumno.ipn.mx>
2020-06-18 03:55:22 -0500

    Eliminar fontawesome
    Eliminar por completo fontawesome.

--- SASTRE/sastre/settings.py
+++ SASTRE/sastre/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'usuario.apps.UsuarioConfig',
-    'fontawesome',
     'widget_tweaks',
     'inscripcionTesis',
     'evaluaciones',
